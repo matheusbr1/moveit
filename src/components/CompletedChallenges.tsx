65fdde9b5882af123033c26e485cca6d477d4fd9
Matheus <=>
2021-02-23 12:25:04 -0300

    Feat: Project migrated to nextJS

--- src/components/CompletedChallenges.tsx
+++ src/components/CompletedChallenges.tsx
@@ -4,7 +4,7 @@ export function CompletedChallenges() {
   return (
     <div className={styles.completedChallengesContainer}>
       <span>Desafios completos</span>
-      <span>5</span>
+      <span> 5 </span>
     </div>
   )
 }
